7bcc25ed9169f30d7ebba047cfe6f4b4b6130e58
azamat2115 <azamatmaqsudjonovich@gmail.com>
2022-05-24 23:40:20 +0500

    initial vme

---
+++ src/main/java/com/example/webappservice/WebAppServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.webappservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAppServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebAppServiceApplication.class, args);
+    }
+
+}
